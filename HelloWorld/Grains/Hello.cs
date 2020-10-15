1cc6b5112694a43119d0afeb398056cbca8c83d9
Coldairarrow <862520575@qq.com>
2020-10-15 21:37:28 +0800

    使用数据库持久

--- HelloWorld/Grains/Hello.cs
+++ HelloWorld/Grains/Hello.cs
@@ -1,14 +1,24 @@
 ﻿using IGrains;
+using Microsoft.Extensions.Logging;
 using Orleans;
-using Orleans.Concurrency;
 using System;
 using System.Threading.Tasks;
 
 namespace Grains
 {
-    [StatelessWorker]
     public class Hello : Grain, IHello
     {
+        private readonly ILogger _logger;
+        public Hello(ILoggerFactory  loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger(GetType());
+        }
+        public override Task OnActivateAsync()
+        {
+            _logger.LogInformation("激活**********************************");
+
+            return base.OnActivateAsync();
+        }
         public async Task<string> SayHello(string name)
         {
             Console.WriteLine($"{DateTime.Now}:收到 {name} 请求");
--- HelloWorld/SiloHost1/Program.cs
+++ HelloWorld/SiloHost1/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
+using System;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -12,24 +13,57 @@ namespace SiloHost1
 {
     class Program
     {
-        public static Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            return new HostBuilder()
+            var invariant = "System.Data.SqlClient";
+            var connectionString = "Data Source=192.168.56.1;Initial Catalog=Orleans;User Id=sa;Password=123456;";
+            string ip = string.Empty;
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                ip = Environment.GetEnvironmentVariable("IP", EnvironmentVariableTarget.Machine);
+            }
+            else
+            {
+                ip = Environment.GetEnvironmentVariable("IP", EnvironmentVariableTarget.Process);
+            }
+
+            await Host.CreateDefaultBuilder(args)
                 .UseOrleans(builder =>
                 {
                     builder
-                        .UseLocalhostClustering()
+                        .UseAdoNetClustering(options =>
+                        {
+                            options.ConnectionString = connectionString;
+                            options.Invariant = invariant;
+                        })
                         .Configure<ClusterOptions>(options =>
                         {
-                            options.ClusterId = "dev";
-                            options.ServiceId = "dev";
+                            options.ClusterId = "orleans.test";
+                            options.ServiceId = "orleans.test";
                         })
-                        .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                        .Configure<ClusterMembershipOptions>(options =>
+                        {
+                            options.ProbeTimeout = TimeSpan.FromSeconds(3);
+                            options.IAmAliveTablePublishTimeout = TimeSpan.FromSeconds(3);
+                            options.NumVotesForDeathDeclaration = 1;
+                        })
+                        .Configure<EndpointOptions>(options =>
+                        {
+                            options.AdvertisedIPAddress = IPAddress.Parse(ip);
+                            //options.SiloPort = IpHelper.GetFirstAvailablePort();
+                        })
                         .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(Hello).Assembly).WithReferences())
-                        .AddMemoryGrainStorage(name: "ArchiveStorage");
+                        .AddMemoryGrainStorageAsDefault()
+                        .AddAdoNetGrainStorageAsDefault(options =>
+                        {
+                            options.ConnectionString = connectionString;
+                            options.Invariant = invariant;
+                        })
+                        ;
                 })
                 .ConfigureServices(services =>
                 {
+                    services.AddHostedService<Bootstrapper>();
                     services.Configure<ConsoleLifetimeOptions>(options =>
                     {
                         options.SuppressStatusMessages = true;
